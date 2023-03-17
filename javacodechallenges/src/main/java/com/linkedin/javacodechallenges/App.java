7e8934df1e887e606491ac2172366c0991cb1c89
Kyle Dohmen <40638916+kdohmen29@users.noreply.github.com>
2023-03-17 19:28:27 +0000

    passwordCheck

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
+        int numCheck = 0;
+        int upperCheck = 0;
+        int lowerCheck = 0;
+        if (password.length() >= 6) {
+            for (int i = 0; i < password.length(); i++) {
+                if (Character.isDigit(password.charAt(i)))
+                    numCheck++;
+                if (Character.isUpperCase(password.charAt(i)))
+                    upperCheck++;
+                if (Character.isLowerCase(password.charAt(i))) 
+                    lowerCheck++;
+                if(lowerCheck>0 && upperCheck>0 && numCheck>0)
+                    return true;
+            }
+        }
         return false;
     }
 
@@ -18,4 +33,6 @@ public class App {
 
         scanner.close();
     }
+
+
 }
